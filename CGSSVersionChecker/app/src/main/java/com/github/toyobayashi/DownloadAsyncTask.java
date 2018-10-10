787a781f6138fc1f3b0b195209e990308c535eeb
toyobayashi <lifenglin314@outlook.com>
2018-10-10 17:54:40 +0800

    下载文件

---
+++ CGSSVersionChecker/app/src/main/java/com/github/toyobayashi/DownloadAsyncTask.java
@@ -0,0 +1,90 @@
+package com.github.toyobayashi;
+
+import android.os.AsyncTask;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+class DownloadAsyncTask extends AsyncTask<String, Integer, Object> {
+
+    private DownloadAsyncTaskCallback callback;
+    DownloadAsyncTask (DownloadAsyncTaskCallback callback) {
+        this.callback = callback;
+    }
+
+    @FunctionalInterface
+    public interface DownloadAsyncTaskCallback {
+        void call(Exception err, int current, int total);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Object doInBackground(String... args) {
+        String path = args[1];
+        String dir = path.substring(0, path.lastIndexOf(File.separator));
+        String filename = path.substring(path.lastIndexOf(File.separator) + 1);
+
+        File saveDir = new File(dir);
+        if (!saveDir.exists()) saveDir.mkdirs();
+        File file = new File(path);
+
+        try {
+            URL url = new URL(args[0]);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setConnectTimeout(10000);
+            connection.setRequestProperty("User-Agent", "Dalvik/2.1.0 (Linux; U; Android 7.0; Nexus 42 Build/XYZZ1Y)");
+            connection.setRequestProperty("X-Unity-Version", "5.4.5p1");
+            connection.setRequestProperty("Accept-Encoding", "gzip");
+            connection.setRequestProperty("'Connection'", "Keep-Alive");
+            connection.setRequestMethod("GET");
+
+            int contentLength = connection.getContentLength();
+
+            InputStream inputStream = connection.getInputStream();
+            byte[] buffer = new byte[8192];
+            int len;
+            int current = 0;
+            // ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.flush();
+            while ((len = inputStream.read(buffer)) != -1) {
+
+                fos.write(buffer, 0, len);
+                current += len;
+                publishProgress(current, contentLength);
+            }
+            fos.close();
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return e;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return e;
+        }
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+        callback.call(null, values[0], values[1]);
+    }
+
+    @Override
+    protected void onPostExecute(Object res) {
+        super.onPostExecute(res);
+        if (res != null) callback.call((Exception) res, 0, 0);
+    }
+}
--- CGSSVersionChecker/app/src/main/java/com/github/toyobayashi/MainActivity.java
+++ CGSSVersionChecker/app/src/main/java/com/github/toyobayashi/MainActivity.java
@@ -1,15 +1,20 @@
 package com.github.toyobayashi;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button btnCheck;
+    private Button btnDl;
     private TextView txtResVer;
+    private TextView div;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +22,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnCheck = findViewById(R.id.btnCheck);
+        btnDl = findViewById(R.id.btnDl);
         txtResVer = findViewById(R.id.txtResVer);
+        div = findViewById(R.id.div);
+        div.setText(getExternalFilesDir("").getAbsolutePath());
 
         btnCheck.setOnClickListener((v) -> {
 
@@ -33,10 +41,32 @@ public class MainActivity extends AppCompatActivity {
                 btnCheck.setEnabled(true);
             });
         });
+
+        btnDl.setOnClickListener((v) -> {
+            div.setText("0 / 0");
+            btnDl.setEnabled(false);
+            downloadManifest(
+                "http://storage.game.starlight-stage.jp/dl/10045510/manifests/Android_AHigh_SHigh",
+                getExternalFilesDir("").getAbsolutePath() + File.separator + "manifest.lz4",
+                (err, current, total) -> {
+                    if (err != null) {
+                        btnDl.setEnabled(true);
+                        Toast.makeText(MainActivity.this, err.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                    if (current == total) {
+                        btnDl.setEnabled(true);
+                    }
+                    div.setText(current + " / " + total);
+                }
+            );
+        });
     }
 
     private static void doCheck(CheckAsyncTask.CheckAsyncTaskCallback callback) {
         new CheckAsyncTask(callback).execute();
     }
 
+    private static void downloadManifest(String url, String path, DownloadAsyncTask.DownloadAsyncTaskCallback callback) {
+        new DownloadAsyncTask(callback).execute(url, path);
+    }
 }
