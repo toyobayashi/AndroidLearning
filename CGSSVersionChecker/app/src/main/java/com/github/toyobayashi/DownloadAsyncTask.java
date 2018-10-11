60e170eb2cd955d436468d6e4f973fb837add87d
toyobayashi <lifenglin314@outlook.com>
2018-10-11 20:59:27 +0800

    LZ4Decompressor

--- CGSSVersionChecker/app/src/main/java/com/github/toyobayashi/DownloadAsyncTask.java
+++ CGSSVersionChecker/app/src/main/java/com/github/toyobayashi/DownloadAsyncTask.java
@@ -2,12 +2,13 @@ package com.github.toyobayashi;
 
 import android.os.AsyncTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 class DownloadAsyncTask extends AsyncTask<String, Integer, Object> {
@@ -31,12 +32,16 @@ class DownloadAsyncTask extends AsyncTask<String, Integer, Object> {
     protected Object doInBackground(String... args) {
         String path = args[1];
         String dir = path.substring(0, path.lastIndexOf(File.separator));
-        String filename = path.substring(path.lastIndexOf(File.separator) + 1);
+        // String filename = path.substring(path.lastIndexOf(File.separator) + 1);
 
         boolean isDecompress = args[2].equals("true");
 
         File saveDir = new File(dir);
-        if (!saveDir.exists()) saveDir.mkdirs();
+        if (!saveDir.exists()) {
+            if (!saveDir.mkdirs()) {
+                return new IOException("创建目录失败");
+            }
+        }
         File file = new File(path);
         long fileLength = file.length();
 
@@ -47,40 +52,53 @@ class DownloadAsyncTask extends AsyncTask<String, Integer, Object> {
             connection.setRequestProperty("User-Agent", "Dalvik/2.1.0 (Linux; U; Android 7.0; Nexus 42 Build/XYZZ1Y)");
             connection.setRequestProperty("X-Unity-Version", "5.4.5p1");
             connection.setRequestProperty("Accept-Encoding", "gzip");
-            connection.setRequestProperty("Range", fileLength + "-");
             connection.setRequestProperty("Connection", "Keep-Alive");
+            if (fileLength > 0) {
+                connection.setRequestProperty("Range", fileLength + "-");
+            }
             connection.setRequestMethod("GET");
 
             int contentLength = connection.getContentLength();
-            if (fileLength == contentLength) {
-                return null;
-            }
-
-            InputStream inputStream = connection.getInputStream();
             byte[] buffer = new byte[8192];
             int len;
-            int current = 0;
-            // ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            if (fileLength != contentLength) {
 
-            FileOutputStream fos = new FileOutputStream(file);
-            // fos.flush();
-            while ((len = inputStream.read(buffer)) != -1) {
+                InputStream inputStream = connection.getInputStream();
 
-                fos.write(buffer, 0, len);
-                current += len;
-                publishProgress(current, contentLength);
+                int current = 0;
+                // ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+                FileOutputStream fos = new FileOutputStream(file);
+                // fos.flush();
+                while ((len = inputStream.read(buffer)) != -1) {
+
+                    fos.write(buffer, 0, len);
+                    current += len;
+                    publishProgress(current, contentLength);
+                }
+                inputStream.close();
+                fos.close();
+
             }
-            inputStream.close();
-            fos.close();
             connection.disconnect();
 
             if (isDecompress) {
-                return new Exception("todo");
+                FileInputStream lz4ins = new FileInputStream(path);
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                while ((len = lz4ins.read(buffer)) != -1) {
+                    baos.write(buffer, 0, len);
+                }
+                lz4ins.close();
+
+                LZ4Decompressor lz4Decompressor = new LZ4Decompressor();
+                byte[] decBuffer = lz4Decompressor.decompress(baos.toByteArray());
+                baos.close();
+
+                FileOutputStream decfos = new FileOutputStream(path.substring(0, path.lastIndexOf(".")));
+                decfos.write(decBuffer);
+                decfos.close();
             }
 
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-            return e;
         } catch (IOException e) {
             e.printStackTrace();
             return e;
@@ -103,4 +121,5 @@ class DownloadAsyncTask extends AsyncTask<String, Integer, Object> {
             callback.call(null, -2, -2);
         }
     }
+
 }
